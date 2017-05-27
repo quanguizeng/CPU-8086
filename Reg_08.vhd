a38d822d116f1afa30724c92d0a569287b8170c9
Strahor <sandro.sret@gmail.com>
2017-05-27 14:22:13 +0200

    Update Reg_08.vhd

--- Reg_08.vhd
+++ Reg_08.vhd
@@ -1,7 +1,8 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
-USE ieee.std_logic_arith.ALL;
-USE ieee.std_logic_unsigned.ALL;
+--USE ieee.std_logic_arith.ALL;
+--USE ieee.std_logic_unsigned.ALL;
+USE ieee.numeric_std.ALL;
 ENTITY register8 IS
 	PORT(
 		reg_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0); -- input.
@@ -14,26 +15,29 @@ ENTITY register8 IS
 		r_bit : IN STD_LOGIC; -- new 0 bit after left shift
 		shr : IN STD_LOGIC; -- shift right
 		l_bit : IN STD_LOGIC; -- new 7 bit after right shift
-		reg_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0); -- output.
+		reg_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0) -- output
+	);
 	END register8;
 ARCHITECTURE description OF register8 IS
-
+	SIGNAL reg_val : STD_LOGIC_VECTOR(7 DOWNTO 0);
 BEGIN
+	reg_out <= reg_val;
 	process(clk, clr)
 	begin
 		if clr = '1' then 
-			reg_out <= x"00";
+			reg_val <= x"00";
 		elsif rising_edge(clk) then
 			if ld = '1' then
-				reg_out <= reg_in;
+				reg_val <= reg_in;
 			elsif inc = '1' then
-				reg_out <= std_logic_vector( unsigned(reg_out) + 1);
+				reg_val <= STD_LOGIC_VECTOR( UNSIGNED(reg_val) + 1);
 			elsif dec = '1' then
-				reg_out <= std_logic_vector( unsigned(reg_out) - 1);
+				reg_val <= STD_LOGIC_VECTOR( UNSIGNED(reg_val) - 1);
 			elsif shl = '1' then
-				reg_out <= (7 downto 1 => reg_out(6 downto 0), 0 => r_bit);
+				reg_val <= reg_val(6 downto 0) & r_bit;
 			elsif shr = '1' then
-				reg_out <= (7 => l_bit, 6 downto 0 => reg_out(7 downto 1));
+				reg_val <= l_bit & reg_val(7 downto 1);
+			end if;
 		end if;
 	end process;
 END description;
